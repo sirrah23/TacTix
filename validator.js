165c9816c56cc9ab693c7f78d00a113bcd504e39
Harris <hseth93@gmail.com>
2016-05-02 02:34:26 -0400

    Adds row validations for square indices

--- validator.js
+++ validator.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const LOWER_BOUNDARY = 0;
 const UPPER_BOUNDARY = 24;
 
@@ -21,6 +23,26 @@ function withinBoundaries(squares){
   return squares.filter(numWithinBoundaries).length == squares.length;
 }
 
+/**
+ * Given a list of numbers, will determine if all
+ * of them fall within a row on a 5x5 grid, without any gaps.
+ * @param {Array.number} squares
+ * @return {boolean}
+ *
+ */
+function validRow(squares){
+  //Check that all given squares are next to each other
+  //by checking the difference between their positions
+  let squareCopy = squares.slice();
+  squareCopy.sort();
+  let differences = [];
+  for(let i = 0; i < squareCopy.length-1;i++){
+    differences.push(squareCopy[i+1]-squareCopy[i]);
+  }
+  return differences.filter(function(n){return n == 1;}).length == squareCopy.length-1;
+}
+
 module.exports = {
   withinBoundaries,
-}
+  validRow,
+};
