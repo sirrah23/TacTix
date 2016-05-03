5dbdb0f2c52371c8b3812d17211bac45e2ed9e1b
Harris <hseth@gmail.com>
2016-05-02 21:49:01 -0400

    Added validation for number of squares selected

--- Tests/test.js
+++ Tests/test.js
@@ -1,6 +1,24 @@
 var assert = require('chai').assert;
 var validator = require('../validator.js');
 
+describe('Make sure player chose between 1 and 5 squares', function(){
+		it('should return true for 1<= size <= 5', function(){
+				assert.equal(true, validator.validNumberOfSelections([0,5,3,2,1]));
+				assert.equal(true, validator.validNumberOfSelections([0]));
+				assert.equal(true, validator.validNumberOfSelections([18,19]));
+				assert.equal(true, validator.validNumberOfSelections([21,26,27]));
+				assert.equal(true, validator.validNumberOfSelections([1,2,3,4]));
+				assert.equal(true, validator.validNumberOfSelections([13,12,11,10]));
+		})
+
+		it('should return false for size 0 or more than 5', function(){
+				assert.equal(false, validator.validNumberOfSelections([]));
+				assert.equal(false, validator.validNumberOfSelections([0,1,2,3,4,5,6]));
+				assert.equal(false, validator.validNumberOfSelections([25,26,27,28,29,30]));
+		})
+})
+
+
 describe('Check the boundaries of the board', function(){
 	it('should return true for valid square indicies',function(){
 		assert.equal(true,validator.withinBoundaries([0]));
